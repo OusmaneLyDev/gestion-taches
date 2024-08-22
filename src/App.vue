2d92b26cd0c2b5a6a1ccee5806f882b939f85313
Ehya-Ag <ehyaagmohamed23@gmail.com>
2024-08-22 11:32:44 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/Projets.vue'
+</script>
+
+<template>
+  <header>
+      <nav>
+        <RouterLink to="./components/Projets">Taches</RouterLink>
+        <RouterLink to="./components/Taches">Projets</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Projets.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <h1>Projets</h1>
+</template>
+
+<style scoped>
+
+</style>
